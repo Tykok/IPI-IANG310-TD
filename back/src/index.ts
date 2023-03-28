e5fb044527d15d257dae90d5255713511ef02423
Tykok <treportelie12@gmail.com>
2023-03-28 16:07:55 +0200

    [Fix] Add CORS options

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,10 +1,15 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import morgan from 'morgan'
+import cors from 'cors'
 
 import router from './router/router'
 
 const app = express()
+
+const corsOptions = {
+  origin: ['http://localhost:4200'],
+};
 
 // enables environment variables by
 // parsing the .env file and assigning it to process.env
@@ -20,6 +25,7 @@ if (NODE_ENV === 'DEV') {
 }
 
 app.use(express.json())
+app.use(cors(corsOptions));
 app.use('', router)
 
 app.listen(port, () => {
