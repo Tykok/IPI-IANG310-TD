07132c357a3570547ada636e26e1145d0ab727cb
Tykok <treportelie12@gmail.com>
2023-03-29 08:57:25 +0200

    [Fix] Fix error on /api/token and change username of user in seeders

--- back/src/db/seeders/user.seeder.ts
+++ back/src/db/seeders/user.seeder.ts
@@ -4,7 +4,7 @@ import { UserColumn, userTableName } from '../db.constants'
 const lines: UserColumn[] = [
   {
     id: 1,
-    username: '\\x8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918',
+    username: '\\x5edfa2692bdacc5e6ee805c626c50cb44cebb065f092d9a1067d89f74dacd326',
     password: '\\x8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918'
   }
 ]
--- back/src/router/football.router.ts
+++ back/src/router/football.router.ts
@@ -8,31 +8,31 @@ const footballRouter = express.Router()
 footballRouter.post('/', async (req, res) => {
   const club: Football = req.body
   const newClub = await createClub(club)
-  res.status(HttpStatusCode.CREATED).json(newClub)
+  return res.status(HttpStatusCode.CREATED).json(newClub)
 })
 
 footballRouter.get('/', async (req, res) => {
   const allClubs = await getAllClubs()
-  res.status(HttpStatusCode.OK).json(allClubs)
+  return res.status(HttpStatusCode.OK).json(allClubs)
 })
 
 footballRouter.get('/:clubId', async (req, res) => {
   const clubId = Number(req.params.clubId)
   const club = await getClubById(clubId)
-  res.status(HttpStatusCode.OK).json(club)
+  return res.status(HttpStatusCode.OK).json(club)
 })
 
 footballRouter.put('/:clubId', async (req, res) => {
   const clubId = Number(req.params.clubId)
   const club: Football= req.body
   const numberOfRowUpdated = await updateClubById(clubId, club)
-  res.status(HttpStatusCode.CREATED).json(`${numberOfRowUpdated} row(s) updated`)
+  return res.status(HttpStatusCode.CREATED).json(`${numberOfRowUpdated} row(s) updated`)
 })
 
 footballRouter.delete('/:clubId', async (req, res) => {
   const clubId = Number(req.params.clubId)
   const numberOfRowDeleted = await deleteClubById(clubId)
-  res.status(HttpStatusCode.OK).json(`${numberOfRowDeleted} row(s) deleted`)
+  return res.status(HttpStatusCode.OK).json(`${numberOfRowDeleted} row(s) deleted`)
 })
 
 export default footballRouter
